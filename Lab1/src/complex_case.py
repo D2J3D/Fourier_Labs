bbb64d06032a0cf372033c182be37b030bca86a5
D2J3D <120342275+D2J3D@users.noreply.github.com>
2024-04-04 23:04:45 +0300

    Add files via upload

---
+++ Lab1/src/complex_case.py
@@ -0,0 +1,128 @@
+import math
+from numpy import exp, sin, cos, sqrt, pi, inf
+import numpy as np
+import matplotlib.pyplot as plt
+
+def integrator(function, a, b):
+    step = 0.001
+    t = np.linspace(a, b, int((b - a) / step))
+    return sum([function(i) * step for i in t])
+
+
+def fitter(array):
+    for i in range(len(array)):
+        if abs(array[i]) < math.e**(-10):
+            array[i] = 0
+        array[i] = round(array[i], 2)
+    return array    
+
+
+def find_c_k(function, w_n, dt):
+    c_k_real = integrator(lambda t: cos(w_n * t) * function(t), dt[0], dt[1])
+    c_k_im = 1 * integrator(lambda t: sin(w_n * t) * function(t), dt[0], dt[1])
+    return [c_k_real, c_k_im]
+
+def get_c_k(function, T, dt, n):
+    w_n = (2 * pi)/T
+    c_coeffs_real = [0 for i in range(2 * n + 1)]
+    c_coeffs_im = [0 for i in range(2 * n + 1)]
+    for i in range(0, n + 1):
+        c_k = find_c_k(function, 2 * pi * (-n+i) / T, dt)
+        c_coeffs_real[i] = 1 / T * c_k[0]
+        c_coeffs_im[i] = -1 / T * c_k[1]
+        if (len(c_coeffs_real) - i - 1 < len(c_coeffs_real)):
+            c_coeffs_real[len(c_coeffs_real) - i - 1] = 1 / T * c_k[0]
+            c_coeffs_im[len(c_coeffs_im) - i - 1] = 1 / T *  c_k[1]
+    c_coeffs_real[0] /= 2
+    c_coeffs_real = fitter(c_coeffs_real)
+    c_coeffs_im = fitter(c_coeffs_im)
+    # all_complex_coeffs = [complex(c_coeffs_real[j], c_coeffs_im[j]) for j in range(len(c_coeffs_im))]
+    return [c_coeffs_real, c_coeffs_im]
+
+
+
+def create_fourier_exponential(function, T, dt, n):
+    c_coeffs_comp = get_c_k(function, T, dt, n)
+    w_n = (2 * pi)/T
+    c_coeffs_real = c_coeffs_comp[0]
+    c_coeffs_im = c_coeffs_comp[1]
+    approx_real = lambda t: sum([(cos(w_n * (-n + i) * t) * c_coeffs_real[i] - sin(w_n * (-n + i) * t) * c_coeffs_im[i]) for i in range(0, 2 * n + 1)])
+    approx_im = lambda t: sum([(cos(w_n * (-n + i) * t) * c_coeffs_im[i] + c_coeffs_real[i] * sin(w_n * (-n + i) * t)) for i in range(0, 2 * n + 1)])
+    function_approximation = lambda t: complex(approx_real(t), approx_im(t))
+    return function_approximation
+
+
+def original_function(t): 
+    T, R = 8, 5 
+    if -T / 8 <= t < T / 8:
+        return complex(R, (8 * R * t) / T)
+    elif T / 8 <= t < 3 * T / 8: 
+       return complex(2 * R - (8 * R * t) / T, R)
+    elif 3 * T / 8 <= t < 5 * T / 8:
+        return complex(-R, 4 * R - (8 * R * t) / T)
+    elif 5 * T / 8 <= t <= 7 * T / 8:
+        return complex(-6 * R + (8 * R * t) / T, -R)
+
+        
+def save_approx_for_report(N):
+    fig, ax = plt.subplots()
+    T = 8
+    R = 5
+    x_1 = np.linspace(-T/8, 7*T/8, 100)
+    y_1 = [original_function(i) for i in x_1] 
+    fourier_approx_exp = create_fourier_exponential(original_function, T, [-T/8, 7*T/8], N)
+    real_part = [np.real(original_function(i)) for i in x_1]
+    complex_part = [np.imag(original_function(i)) for i in x_1]
+    ax.plot(real_part, complex_part, label='original')
+    y2 = [fourier_approx_exp(x_1[i]) for i in range(len(x_1))]
+    real_approx = [np.real(i) for i in y2]
+    complex_approx = [np.imag(i) for i in y2]
+    ax.plot(real_approx, complex_approx, label='G_N')
+    ax.grid(True)
+    ax.legend()
+    plt.savefig('G_' + str(N) + '_complex-func.jpg')
+
+
+def save_Re(N):
+    fig, ax = plt.subplots()
+    T = 8
+    R = 5
+    x_1 = np.linspace(-T/8, 7*T/8, 100)
+    y_1 = [np.real(original_function(i)) for i in x_1] 
+    fourier_approx_exp = create_fourier_exponential(original_function, T, [-T/8, 7*T/8], N)
+    real_part = [np.real(original_function(i)) for i in x_1]
+    ax.plot(x_1, y_1)
+    ax.plot(x_1, real_part, label='Re(f(t))')
+    y2 = [fourier_approx_exp(x_1[i]) for i in range(len(x_1))]
+    real_approx = [np.real(i) for i in y2]
+    ax.plot(x_1, real_approx, label='Re(G_N)')
+    ax.grid(True)
+    ax.legend()
+    plt.savefig('G_' + str(N) + '_complex-func_real_part.jpg')
+
+
+def save_Im(N):
+    fig, ax = plt.subplots()
+    T = 8
+    R = 5
+    x_1 = np.linspace(-T/8, 7*T/8, 100)
+    y_1 = [np.real(original_function(i)) for i in x_1] 
+    fourier_approx_exp = create_fourier_exponential(original_function, T, [-T/8, 7*T/8], N)
+    complex_part = [np.imag(original_function(i)) for i in x_1]
+    ax.plot(x_1, complex_part, label='Im f(t)')
+    y2 = [fourier_approx_exp(x_1[i]) for i in range(len(x_1))]
+    complex_approx = [np.imag(i) for i in y2]
+    ax.plot(x_1, complex_approx, label='Im G_N')
+    ax.grid(True)
+    ax.legend()
+    plt.savefig('G_' + str(N) + '_complex-func_imag_part.jpg')
+
+
+
+if __name__ == "__main__":
+    n_s = [1, 2, 3,5, 10]
+    for i in range(len(n_s)):
+        save_approx_for_report(n_s[i])
+        save_Re(n_s[i])
+        save_Im(n_s[i])
+
---
+++ Lab1/src/real_case.py
@@ -0,0 +1,151 @@
+import math
+from numpy import sin, cos, exp, pi
+import numpy as np
+import matplotlib.pyplot as plt
+
+def integrator(function, a, b):
+    step = 0.001
+    t = np.linspace(a, b, int((b - a) / step))
+    return sum([function(i) * step for i in t])
+
+
+def fitter(array):
+    for i in range(len(array)):
+        if abs(array[i]) < math.e**(-10):
+            array[i] = 0
+        array[i] = round(array[i], 2)
+    return array
+
+
+def module(num):
+    return np.real(np.sqrt(num * np.conjugate(num)))
+
+
+def find_a_k(function, w_n, dt):
+    a_func = lambda t: function(t) * cos(w_n * t)
+    a_k = integrator(a_func, dt[0], dt[1])
+    if abs(a_k) < math.e**(-10):
+        return 0.0
+    return a_k
+
+
+def find_b_k(function, w_n, dt):
+    b_func = lambda t: function(t) * sin(w_n * t)
+    b_k = integrator(b_func, dt[0], dt[1])
+    if abs(b_k) < math.e**(-10):
+        return 0.0
+    return b_k
+
+
+def find_c_k(function, w_n, dt):
+    c_k_real = integrator(lambda t: cos(w_n * t) * function(t), dt[0], dt[1])
+    c_k_im = 1 * integrator(lambda t: sin(w_n * t) * function(t), dt[0], dt[1])
+    return [c_k_real, c_k_im]
+
+
+def get_c_k(function, T, dt, n):
+    w_n = (2 * pi)/T
+    c_coeffs_real = [0 for i in range(2 * n + 1)]
+    c_coeffs_im = [0 for i in range(2 * n + 1)]
+    for i in range(0, n + 1):
+        c_k = find_c_k(function, 2 * pi * (-n+i) / T, dt)
+        c_coeffs_real[i] = 1 / T * c_k[0]
+        c_coeffs_im[i] = -1 / T * c_k[1]
+        if (len(c_coeffs_real) - i - 1 < len(c_coeffs_real)):
+            c_coeffs_real[len(c_coeffs_real) - i - 1] = 1 / T * c_k[0]
+            c_coeffs_im[len(c_coeffs_im) - i - 1] = 1 / T *  c_k[1]
+    c_coeffs_real[0] /= 2
+    c_coeffs_real = fitter(c_coeffs_real)
+    c_coeffs_im = fitter(c_coeffs_im)
+    return [c_coeffs_real, c_coeffs_im]
+
+
+def create_fourier_exponential(function, T, dt, n):
+    c_coeffs_comp = get_c_k(function, T, dt, n)
+    w_n = (2 * pi)/T
+    c_coeffs_real = c_coeffs_comp[0]
+    c_coeffs_im = c_coeffs_comp[1]
+    approx_real = lambda t: sum([(cos(w_n * (-n + i) * t) * c_coeffs_real[i] - sin(w_n * (-n + i) * t) * c_coeffs_im[i]) for i in range(0, 2 * n + 1)])
+    approx_im = lambda t: sum([(cos(w_n * (-n + i) * t) * c_coeffs_im[i] + c_coeffs_real[i] * sin(w_n * (-n + i) * t)) for i in range(0, 2 * n + 1)]) # actually, just a zero, because function is Real only
+    function_approximation = lambda t: approx_real(t)
+    return function_approximation
+
+
+def create_fourier_classical(function, T, dt, n):
+    a_coeffs = [2 / T * find_a_k(function, 2 * pi * k/T, dt) for k in range(0, n+1)]
+    a_coeffs[0] /= 2
+    b_coeffs = [2 / T * find_b_k(function, 2 * pi * k/T, dt) for k in range(0, n+1)]
+    approx = lambda t: sum([a_coeffs[i] * cos(2 * pi * i * t/T) + b_coeffs[i] * sin(2 * pi * i * t/T) for i in range(n)] )
+    return approx
+
+
+def get_coeffs_real(function, T, dt, n):
+    a_coeffs = [2 / T * find_a_k(function, 2 * pi * k/T, dt) for k in range(0, n+1)]
+    a_coeffs[0] /= 2
+    b_coeffs = [2 / T * find_b_k(function, 2 * pi * k/T, dt) for k in range(0, n+1)]
+    return a_coeffs, b_coeffs
+
+def check_Parseval_real(function, T, dt, a_k, b_k):
+    step = 0.00001
+    a, b = dt[0], dt[1]
+    a_k[0] *= 2 / np.sqrt(2)
+    t = np.linspace(a, b, int((b - a) / step))
+    left_part = 2 / T * sum([function(i) * function(i) * step for i in t])
+    right_part = sum([a_k[i]**2 + b_k[i]**2 for i in range(len(b_k))])
+    return left_part, right_part
+
+
+def check_Parseval_exponential(function, T, dt, c_k):
+    step = 0.00001
+    a, b = dt[0], dt[1]
+    t = np.linspace(a, b, int((b - a) / step))
+    left_part = 2 / T * sum([function(i) * np.conjugate(function(i)) * step for i in t])
+    right_part = sum([(c_k[0][i]**2 + c_k[1][i]**2) for i in range(len(c_k))])
+    return left_part, right_part
+
+def original_function(t):
+    return t + t**3 + 2**t
+
+
+
+def save_Fn(original_function, N):
+    fig, ax = plt.subplots()
+    x = np.linspace(1, 10, 100)
+    y_1 = [original_function(i) for i in  x_1]
+    fourier_approx = create_fourier_classical(original_function, 4, [-2, 2], N)
+    y_2 = [fourier_approx(x_1[i]) for i in range(len(x))]
+    ax.plot(x, y_1, label='original')
+    ax.plot(x, y_2, label='F_n = ' + str(N))
+    ax.grid(True)
+    ax.legend()
+    plt.savefig('F_' + str(N) + '_classical.jpg')
+
+
+def save_Gn(original_function, N):
+    fig, ax = plt.subplots()
+    x = np.linspace(-2, 2, 100)
+    y_1 = [original_function(i) for i in x_1]
+    fourier_approx = create_fourier_exponential(original_function, 4, [-2, 2], N)
+    y_2 = [fourier_approx(x_1[i]) for i in range(len(x))]
+    ax.plot(x, y_1, label='original')
+    ax.plot(x, y_2, label='G_n = ' + str(N))
+    ax.grid(True)
+    ax.legend()
+    plt.savefig('G_' + str(N) + '_exponential.jpg')
+    
+
+
+if __name__ == "__main__":
+    fig, ax = plt.subplots()
+    x_1 = np.linspace(-2, 2, 100)
+    y_1 = [original_function(i) for i in x_1]
+    # all_coeffs_real = get_coeffs_real(original_function, 4, [-2 , 2], 5)
+    all_coeffs_real = get_coeffs_real(original_function, 4, [-2, 2], 25)
+    all_coeffs_exponential = get_c_k(original_function, 4, [-2, 2], 25)
+
+    print("info ", check_Parseval_real(original_function, 4, [-2, 2], all_coeffs_real[0], all_coeffs_real[1]))
+    print("info ", check_Parseval_exponential(create_fourier_exponential(original_function, 4, [-2, 2], 25), 4, [-2, 2], all_coeffs_exponential))
+    
+    # print(all_coeffs_real)
+    # print(all_coeffs_exponential)
+    # fourier_approx_exp = create_fourier_exponential(original_function, 6, [-3, 3], 8)
